e31d5ffd71c17060ad1ede0d4b7b80c7ea3dd2e6
David Perez <david@closemarketing.es>
2015-09-04 16:18:26 +0200

    First version

---
+++ debug-mo-translations.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Plugin Name: Debug MO Translations
+ * Description: Get translation data: language, files, possible problems.
+ * Plugin URI:  https://github.com/closemarketing/debug-mo-translations
+ * Version:     1.0
+ * Author:      closemarketing
+ * Author URI:  https://www.closemarketing.es/
+ * Licence:     GPL 2
+ * License URI: http://opensource.org/licenses/GPL-2.0
+ */
+
+/**
+ * Main controller for this plugin.
+ *
+ * @author toscho
+ */
+class Debug_MO_Translations_Controller {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'setup' ), -1 );
+	}
+
+	/**
+	 * Create objects and register callbacks.
+	 *
+	 * @wp-hook plugins_loaded
+	 * @return  void
+	 */
+	public function setup() {
+
+		if ( ! current_user_can( 'update_core' ) )
+			return;
+
+		$logger = new Debug_MO_Translations_Logger();
+		$output = new Debug_MO_Translations_Output( $logger );
+
+		add_filter(
+			'override_load_textdomain',
+			array ( $logger, 'log_file_load' ),
+			10,
+			3
+		);
+
+		add_action( 'shutdown', array ( $output, 'show' ), 0 );
+	}
+}
+
+/**
+ * Logger. Collects information about the load attempts.
+ *
+ * @author toscho
+ */
+class Debug_MO_Translations_Logger {
+
+	/**
+	 * List of log entries.
+	 *
+	 * @type array
+	 */
+	protected $log = array();
+
+	/**
+	 * Store log data.
+	 *
+	 * @wp-hook override_load_textdomain
+	 * @param   bool $false FALSE, passed though
+	 * @param   string $domain Text domain
+	 * @param   string $mofile Path to file.
+	 * @return  bool
+	 */
+	public function log_file_load( $false, $domain, $mofile ) {
+
+		// DEBUG_BACKTRACE_IGNORE_ARGS is available since 5.3.6
+		if ( version_compare(PHP_VERSION, '5.3.6') >= 0 )
+			$trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
+		else
+			$trace = debug_backtrace();
+
+		$this->log[] = array (
+			'caller' => $trace[ 4 ], // entry 4 is the calling file
+			'domain' => $domain,
+			'mofile' => $mofile,
+			'found'  => file_exists( $mofile ) ? round( filesize( $mofile ) / 1024, 2 ): FALSE
+		);
+
+		return $false;
+	}
+
+	/**
+	 * Return complete log.
+	 *
+	 * Used by the model.
+	 *
+	 * @return array
+	 */
+	public function get_log() {
+		return $this->log;
+	}
+}
+
+/**
+ * Print a pretty output.
+ *
+ * @author toscho
+ */
+class Debug_MO_Translations_Output {
+
+	/**
+	 * The data model
+	 *
+	 * @type Debug_MO_Translations_Logger
+	 */
+	protected $logger;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Debug_MO_Translations_Logger $logger
+	 */
+	public function __construct( Debug_MO_Translations_Logger $logger ) {
+		$this->logger = $logger;
+	}
+
+	/**
+	 * Create and print the output.
+	 *
+	 * @wp-hook shutdown
+	 * @return  void
+	 */
+	public function show() {
+
+		list( $name, $version )
+			= get_file_data( __FILE__, array ( 'Plugin Name', 'Version' ) );
+
+		$data = array (
+			"<b>$name (Version $version)</b>\n",
+			"Locale: " . esc_html( get_locale() ) . "\n"
+		);
+		$data += $this->get_log();
+
+		print '<div id="wpcontent"><pre>' . join( "\n", $data ) . '</pre></div>';
+	}
+
+	/**
+	 * Get log data from model.
+	 *
+	 * @return array
+	 */
+	protected function get_log() {
+
+		$logs = $this->logger->get_log();
+
+		if ( empty ( $logs ) )
+			return array ( 'No MO file loaded or logged.' );
+
+		$out = array ();
+
+		foreach ( $logs as $log ) {
+			$out[] = $this->get_formatted_log( $log );
+		}
+
+		return $out;
+	}
+
+	/**
+	 * Prettify a log entry
+	 *
+	 * @param  array $log
+	 * @return string
+	 */
+	protected function get_formatted_log( Array $log ) {
+
+        if(!isset($log[ 'caller' ][ 'file' ]) ) return '';
+
+		return sprintf( '
+Domain:    %1$s
+File:      %2$s (%3$s)
+Called in: %4$s line %5$s %6$s',
+			$log[ 'domain' ],
+			$log[ 'mofile' ],
+			$log[ 'found' ] ? $log[ 'found' ] . 'kb' : '<b>not found</b>',
+			$log[ 'caller' ][ 'file' ],
+			$log[ 'caller' ][ 'line' ],
+			$log[ 'caller' ][ 'function' ]
+		);
+
+	}
+}
+
+new Debug_MO_Translations_Controller;
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Debug MO Translations ===
+Contributors: closemarketing
+Tags: bootstrap, shortcodes, content, ui, bootstrap helper
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZYGC6AT5JFQVE
+Requires at least: 3.0
+Tested up to: 4.3
+Stable tag: 1.0
+Version: 1.0
+
+Debugs all translated files that are loaded and not in Wordpress.
+
+== Description ==
+It gives you more information about all domains and translated mo files loaded in the actual installation, so you can debug what's the problem with a plugin, theme, etc.
+
+It's a fork, improved and updated plugin from [Debug Translations Marketpress Plugin](https://marketpress.com/product/debug-translations/)
+
+[Official Repository Github](https://github.com/closemarketing/debug-mo-translations).
+Fork and add make suggestions to the plugin!
+
+Others Plugins:
+- [Gravity Forms CRM Addon](http://codecanyon.net/item/gravity-forms-crm-addon/10521695)
+- [Gravity Forms in Spanish](https://wordpress.org/plugins/gravityforms-es/)
+- [Clean HTML in Editor](https://wordpress.org/plugins/clean-html/)
+- [Send SMS to Wordpress Users via Arsys](https://wordpress.org/plugins/send-sms-arsys/)
+
+Made by [Closemarketing](https://www.closemarketing.es/)
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your
+WordPress installation and then activate the Plugin from Plugins page.
+
+
+== Developers ==
+[Official Repository Github](https://github.com/closemarketing/debug-mo-translations)
+
+== Changelog ==
+
+= 1.0 =
+*	First released, created one shortcode.
+
+== Links ==
+
+*	[Closemarketing](https://www.closemarketing.es/)
